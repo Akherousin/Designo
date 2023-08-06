104dfeec855a361a91098ab7a67266829c39b5dd
Arthur Badin <akherousin94@gmail.com>
2023-08-07 00:48:53 +0600

    minor style changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,13 @@
+import CardLink from '@/components/CardLink';
 import styles from './page.module.css';
 import Hero from '@/components/Hero';
+import CardLinkSection from '@/components/CardLinkSection';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Hero />
+      <CardLinkSection />
     </main>
   );
 }
