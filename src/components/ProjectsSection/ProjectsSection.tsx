fc18531ba299ddf797574c348acbdf5596aa014f
Arthur Badin <akherousin94@gmail.com>
2023-08-21 18:40:54 +0600

    add ProjectSection component

---
+++ src/components/ProjectsSection/ProjectsSection.tsx
@@ -0,0 +1,26 @@
+import { PROJECT } from '@/data';
+import CardProject from '../CardProject/CardProject';
+import styles from './ProjectsSection.module.css';
+
+interface ProjectSectionProps {
+  projects: PROJECT[];
+}
+
+function ProjectSection({ projects }: ProjectSectionProps) {
+  return (
+    <section className={styles.projects}>
+      {projects.map((project) => {
+        return (
+          <CardProject
+            key={project.title}
+            image={project.image}
+            title={project.title}
+            text={project.text}
+          />
+        );
+      })}
+    </section>
+  );
+}
+
+export default ProjectSection;
