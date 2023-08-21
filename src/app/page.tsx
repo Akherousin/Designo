0eefe7b8d6898809f9024ad718c743f2f953596c
Arthur Badin <akherousin94@gmail.com>
2023-08-21 18:41:33 +0600

    rewrite using separator

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
-import CardLink from '@/components/CardLink';
-import styles from './page.module.css';
 import Hero from '@/components/Hero';
 import CardLinkSection from '@/components/CardLinkSection';
+import CardFeatureSection from '@/components/CardFeatureSection';
+import CallToAction from '@/components/CallToAction';
+import Separator from '@/components/Separator/Separator';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <>
       <Hero />
+      <Separator size="small" />
       <CardLinkSection />
-    </main>
+      <Separator size="big" />
+      <CardFeatureSection />
+      <Separator size="big" />
+      <CallToAction />
+    </>
   );
 }
