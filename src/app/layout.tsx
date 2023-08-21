20d34f5f2c723f6c34568af3a7dd002f6c4b5018
Arthur Badin <akherousin94@gmail.com>
2023-08-21 18:41:19 +0600

    add Footer to all pages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './reset.css';
 import './globals.css';
 import Header from '@/components/Header';
+import Footer from '@/components/Footer';
 import type { Metadata } from 'next';
 import { Jost } from 'next/font/google';
 
@@ -20,7 +21,8 @@ export default function RootLayout({
     <html lang="en">
       <body className={jost.className}>
         <Header />
-        {children}
+        <main>{children}</main>
+        <Footer />
       </body>
     </html>
   );
