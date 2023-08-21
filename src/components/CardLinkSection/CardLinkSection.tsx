afcd43b0ef7a99a9ce9021790db6baf593944b7b
Arthur Badin <akherousin94@gmail.com>
2023-08-21 18:40:27 +0600

    add types

--- src/components/CardLinkSection/CardLinkSection.tsx
+++ src/components/CardLinkSection/CardLinkSection.tsx
@@ -3,8 +3,19 @@ import appDesignImg from '@/assets/home/mobile/image-app-design.jpg';
 import graphicDesignImg from '@/assets/home/mobile/image-graphic-design.jpg';
 import CardLink from '../CardLink';
 import styles from './CardLinkSection.module.css';
+import { StaticImageData } from 'next/image';
 
-const LINKS = [
+interface Link {
+  title: string;
+  href: 'web-design' | 'app-design' | 'graphic-design';
+  image: string | StaticImageData;
+}
+
+interface CardLinkSectionProps {
+  excludeHref?: Link['href'];
+}
+
+const LINKS: Link[] = [
   { title: 'Web Design', href: 'web-design', image: webDesignImg },
 
   { title: 'App Design', href: 'app-design', image: appDesignImg },
@@ -12,12 +23,14 @@ const LINKS = [
   { title: 'Graphic Design', href: 'graphic-design', image: graphicDesignImg },
 ];
 
-function CardLinkSection() {
+function CardLinkSection({ excludeHref }: CardLinkSectionProps) {
   return (
     <section className={styles.section}>
-      {LINKS.map(({ title, href, image }) => (
-        <CardLink key={title} href={href} title={title} image={image} />
-      ))}
+      {LINKS.filter((link) => link.href !== excludeHref).map(
+        ({ title, href, image }) => (
+          <CardLink key={title} href={href} title={title} image={image} />
+        )
+      )}
     </section>
   );
 }
